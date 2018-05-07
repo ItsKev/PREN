7c14b5d65fbd4431b101a50cbf3ba8c947b80f2c
Larissa <l.schuler.94@gmail.com>
2018-05-07 14:44:04 +0200

    Show and update position.

--- Website/hello_world.py
+++ Website/hello_world.py
@@ -1,6 +1,6 @@
-import sqlite3
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import json
+from RaspberryPi import WebsiteHandler
 
 app = Flask(__name__)
 
@@ -8,6 +8,14 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     print(request.method)
+
+    myfile = open('mydata.json').read()
+    mydata = json.loads(myfile)
+    lastJsonentry = mydata[len(mydata) - 1]
+    lastX = lastJsonentry["x"]
+    lastZ = lastJsonentry["z"]
+    print("The newest coordinates are: \nx: ", lastX, " z: ", lastZ)
+
     if request.method == 'POST':
         if request.form.get('Start') == 'Start':
             # @ToDo Start method
@@ -15,12 +23,27 @@ def hello_world():
         elif request.form.get('Stop') == 'Stop':
             # @ToDO Stop method
             print('Stop')
-        elif request.form.get('dataview') == 'dataview':
-            return render_template('dataView.html')
-    else:
-        print('Error')
+        # elif request.form.get('Position') == 'Position':
+        #     # @ToDo get Position
+        #     myfile = open('mydata.json').read()
+        #     mydata = json.loads(myfile)
+        #     lastJsonentry = mydata[len(mydata) - 1]
+        #     lastX = lastJsonentry["x"]
+        #     lastZ = lastJsonentry["z"]
+        #     print("The newest coordinates are: \nx: ", lastX, " z: ", lastZ)
 
-    return render_template('home.html')
+            # x, z = WebsiteHandler.WebsiteHandler.start_updating_coordinates()
+        else:
+            print('Error')
+
+    return render_template('home.html', x=lastX, z=lastZ)
+
+
+@app.route('/newPosition')
+def writeNewPosition(x, z):
+    newX = x;
+    newZ = z;
+    # @ToDo
 
 
 if __name__ == '__main__':
